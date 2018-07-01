decef934f84ad7f97e085a226cd0a4655e98fa94
Ashwin Ramanujam <gramanuj@umich.edu>
2018-07-01 03:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string)
 }
 function sayHiToGrandma(string)
 {
-  {if (string.toUpperCase() === string)
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  if (string.toUpperCase() === string)
     return "YES INDEED!"}
     {if (string.toLowerCase() === string)
     return "I can't hear you!"}
